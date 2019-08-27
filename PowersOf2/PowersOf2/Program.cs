86b38a319af94d29a4311b7cdcde755fb92e58ca
Marc Honegger <homel2003@gmail.com>
2019-08-27 20:11:07 +0200

    PowersOf2

---
+++ PowersOf2/PowersOf2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PowersOf2
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.ResetColor();
+                Console.WriteLine("PowerOf2App");
+                Console.WriteLine("---------------\n\r");
+
+                Console.Write("Exponent: ");
+                if (!int.TryParse(Console.ReadLine(), out var exponent))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Is not a natural number");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                var s = "";
+                for (var i = 0; i < exponent; i++) s += Math.Pow(2, i) + " ";
+
+                Console.WriteLine(s);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n\r c to clear ");
+                if (Console.ReadKey().Key == ConsoleKey.C)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
