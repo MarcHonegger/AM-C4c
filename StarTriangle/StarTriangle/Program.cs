2e828a3f706c035eb541d3cba1d976394cee33ae
Marc Honegger <homel2003@gmail.com>
2019-08-27 20:46:52 +0200

    small Things deleted

---
+++ StarTriangle/StarTriangle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace StarTriangle
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.ResetColor();
+                Console.WriteLine("StarTriangle");
+                Console.WriteLine("---------------\n\r");
+
+                Console.Write("Size: ");
+                if (!uint.TryParse(Console.ReadLine(), out var size))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Is not a natural number");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                for (var i = 0; i < size; i++)
+                {
+                    var s = "";
+                    var spaces = size - 1 - i;
+                    for (var j = 0; j < spaces; j++) s += " ";
+
+                    var stars = 1 + 2 * i;
+                    for (var j = 0; j < stars; j++) s += "*";
+                    Console.WriteLine(s);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n\r c to clear ");
+                if (Console.ReadKey().Key == ConsoleKey.C)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
