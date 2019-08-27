e8425b414ac7ac8ff49ed674870dca16efb49d49
Marc Honegger <homel2003@gmail.com>
2019-08-27 19:16:32 +0200

    First and Seocnd Project

---
+++ GreetApp/GreetApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GreetApp
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.ResetColor();
+                Console.WriteLine("GreetApp");
+                Console.WriteLine("---------------\n\r");
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.WriteLine("Dear " + name);
+
+                
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n\r c to clear ");
+                if (Console.ReadKey().Key == ConsoleKey.C)
+                {
+                    Console.Clear();
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ PossibleCalculations/PossibleCalculations/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PossibleCalculations
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.ResetColor();
+
+                Console.WriteLine("PossibleCalculations");
+                Console.WriteLine("---------------\n\r");
+
+                Console.Write("Number One: ");
+                var line1 = Console.ReadLine();
+                Console.Write("Number Two: ");
+                var line2 = Console.ReadLine();
+
+                if (!uint.TryParse(line1, out var number1) || !uint.TryParse(line2, out var number2))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("At least one is not a natural number");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                double double1 = number1;
+                double double2 = number2;
+
+                var resolutions = new List<double>()
+                {
+                    (double1 + double2),
+                    (double1 - double2),
+                    (double2 - double1),
+                    (double1 * double2),
+                    (double1 / double2),
+                    (double2 / double1),
+                };
+
+                foreach (var r in resolutions)
+                {
+                    Console.WriteLine(r);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n\r c to clear ");
+                if (Console.ReadKey().Key == ConsoleKey.C)
+                {
+                    Console.Clear();
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
