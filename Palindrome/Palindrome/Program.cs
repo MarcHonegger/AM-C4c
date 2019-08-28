3793efe3ac1a86bedc30d3e2aff003aeddd10bdc
Marc Honegger <homel2003@gmail.com>
2019-08-28 11:32:53 +0200

    Finish Prime 1to1441

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.ResetColor();
+
+                Console.WriteLine("Random");
+                Console.WriteLine("---------------\n\r");
+
+                Console.Write("Write a series of Number: ");
+
+                var input = Console.ReadLine();
+                var output = "";
+
+                if (input is null)
+                    continue;
+
+                for (var i = 0; i < input.Length; i++) output += input[input.Length - 1 - i];
+
+                Console.WriteLine(output);
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(output);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n\r c to clear ");
+                if (Console.ReadKey().Key == ConsoleKey.C)
+
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
---
+++ Prime1To1441/Prime1To1441/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prime1To1441
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Clear();
+            Console.ResetColor();
+
+            Console.WriteLine("Random");
+            Console.WriteLine("---------------\n\r");
+
+            Console.Write(2 + ", ");
+            for (var i = 3; i < 1441; i += 2)
+            {
+                var isPrime = true;
+
+                for (var j = 3; j < i; j+=2)
+                    if (i % j == 0)
+                        isPrime = false;
+                
+                if (isPrime)
+                    Console.Write(i + ", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
